224a4d7cc1aa176b09c68e38b24408fff660da53
Stephen Thirlwall <sdt@dr.com>
2015-03-08 20:35:57 +1100

    Part one of step 2

---
+++ sdt-perl/lib/MAL/Environment.pm
@@ -0,0 +1,43 @@
+package MAL::Environment;
+use 5.20.0;
+use warnings;
+
+use Moo;
+use Types::Standard qw( HashRef InstanceOf );
+
+use Function::Parameters qw( :strict );
+
+has data => (
+    is => 'ro',
+    isa => HashRef,
+    default => sub { { } },
+);
+
+has outer => (
+    is => 'ro',
+    isa => InstanceOf['MAL::Environment'],
+    predicate => 1,
+);
+
+method set($key, $value) {
+    $self->data->{$key} = $value;
+}
+
+method find($key) {
+    if (exists $self->data->{$key}) {
+        return $self;
+    }
+    if ($self->has_outer) {
+        return $self->outer->find($key);
+    }
+    return;
+}
+
+method get($key) {
+    if (my $env = $self->find($key)) {
+        return $env->data->{$key};
+    }
+    die "Undefined symbol $key\n";
+}
+
+1;
---
+++ sdt-perl/lib/MAL/Object/BuiltIn.pm
@@ -0,0 +1,27 @@
+package MAL::Object::BuiltIn;
+use 5.20.0;
+use warnings;
+
+use parent qw( MAL::Object );
+
+use Function::Parameters qw( :strict );
+
+method new($class: $name, $subref, $return_type) {
+    bless {
+        name => $name,
+        return_type => $return_type,
+        subref => $subref,
+    }, $class;
+}
+
+method apply($args) {
+    my @argvals = map { $_->value } $args->as_list;
+    my $ret = $self->{subref}->(@argvals);
+    return $self->{return_type}->new($ret);
+}
+
+method to_string {
+    return $self->{name};
+}
+
+1;
