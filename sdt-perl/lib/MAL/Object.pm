9fc6a357ad253df9acb648cff24d54dd5c031aaf
Stephen Thirlwall <sdt@dr.com>
2015-03-08 20:35:57 +1100

    Part one of step 2

--- sdt-perl/lib/MAL/Object.pm
+++ sdt-perl/lib/MAL/Object.pm
@@ -9,7 +9,7 @@ use Sub::Name qw( subname );
 {
     my @constants = qw( False Nil True );
     my @scalars   = qw( Integer Keyword String Symbol );
-    my @compounds = qw( Hash Pair Vector );
+    my @compounds = qw( BuiltIn Hash Pair Vector );
     my @types     = (@constants, @scalars, @compounds);
 
     fun pkg($sym, $pkg = __PACKAGE__) {
