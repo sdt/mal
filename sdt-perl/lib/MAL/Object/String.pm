5b741a0af90a1834c0640c423e8cf49d3e329da5
Stephen Thirlwall <sdt@dr.com>
2015-03-09 17:21:41 +1100

    Implement print_readably param on pr_str

--- sdt-perl/lib/MAL/Object/String.pm
+++ sdt-perl/lib/MAL/Object/String.pm
@@ -5,11 +5,14 @@ use warnings;
 use parent qw( MAL::Object::Scalar );
 use Function::Parameters qw( :strict );
 
-method to_string {
+method to_string($readable = 0) {
     my $value = $self->value;
-    $value =~ s/"/\\"/g;
-    $value =~ s/\n/\\n/g;
-    return '"' . $value . '"';
+    if ($readable) {
+        $value =~ s/"/\\"/g;
+        $value =~ s/\n/\\n/g;
+        return '"' . $value . '"';
+    }
+    return $value;
 }
 
 method equal($rhs) {
