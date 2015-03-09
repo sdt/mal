d756c76edc3f985607f6c44408bcf6cc5e7d6782
Stephen Thirlwall <sdt@dr.com>
2015-03-09 14:41:47 +1100

    Split tokeniser into separate module

--- sdt-perl/lib/MAL/Reader.pm
+++ sdt-perl/lib/MAL/Reader.pm
@@ -4,73 +4,7 @@ use warnings;
 
 use Function::Parameters qw( :strict );
 use MAL::Object;
-
-package Reader {
-    use Moo;
-    use Types::Standard qw( ArrayRef Str );
-
-    has input => (
-        is => 'ro',
-        isa => Str,
-    );
-
-    method peek { return $self->_tokens->[0]->{value}; }
-    method empty { return scalar @{ $self->_tokens } == 0; }
-    method next {
-        my $token = $self->peek;
-        shift(@{ $self->_tokens });
-        return $token;
-    }
-
-    method debug($msg) {
-#        use Data::Dumper::Concise; print STDERR $msg, Dumper($self->_tokens);
-    }
-
-    method token_error($msg, $pos = undef) {
-        $pos //= $self->_tokens->[0]->{position};
-        my $inset = $pos + 1;
-        my $input = $self->input;
-        die sprintf("Error: $msg\n$input\n%${inset}s\n", '^');
-    }
-
-    has _tokens => (
-        is => 'lazy',
-        isa => ArrayRef,
-        builder => 1,
-    );
-
-    method _build__tokens {
-        my $re = qr(
-            [\s,]*
-            (   ~@
-            |   [\[\]{}()'`~^@]
-            |   "(?:\\.|[^\\"])*"
-            |   ;.*
-            |   [^\s\[\]{}('"`,;)]*
-            )
-        )x;
-        my @tokens;
-        my $input = $self->input;
-        while ($input =~ m/$re/g) {
-            if (length($1) == 0) {
-                if (pos($input) != length($input)) {
-                    if (substr($input, pos($input), 1) eq '"') {
-                        die "Expected '\"', got EOF\n";
-                    }
-                    $self->token_error('unexpected input', pos($input));
-                }
-                else {
-                    next;
-                }
-            }
-            my $token = { value => $1, position => $-[0] };
-            push(@tokens, $token);
-        }
-
-        @tokens = grep { $_->{value} !~ /^;/ } @tokens; # strip comments
-        return \@tokens;
-    }
-};
+use MAL::Tokeniser;
 
 fun _read_atom($reader) {
     $reader->debug('_read_atom');
@@ -182,7 +116,7 @@ fun _read_form($reader) {
 }
 
 method read_str($class: $input) {
-    my $reader = Reader->new(input => $input);
+    my $reader = MAL::Tokeniser->new(input => $input);
     if ($reader->empty) {
         return MAL::Object->nil;
     }
