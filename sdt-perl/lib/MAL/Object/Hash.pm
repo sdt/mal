78fc55333469ecfef9a8443c9a3872779632e1e8
Stephen Thirlwall <sdt@dr.com>
2015-03-12 21:28:00 +1100

    Implement hashmap operations

--- sdt-perl/lib/MAL/Object/Hash.pm
+++ sdt-perl/lib/MAL/Object/Hash.pm
@@ -6,6 +6,10 @@ use parent qw( MAL::Object );
 
 use Function::Parameters qw( :strict );
 
+fun make_key($key) {
+    return $key->to_string(1);
+}
+
 method new($class: @items) {
     die 'Odd number of elements in hash' if @items % 2;
     my %hash;
@@ -14,7 +18,7 @@ method new($class: @items) {
         die "Keys to hash must be keywords or strings"
             unless $key->is_string || $key->is_keyword;
         my $value = shift @items;
-        $hash{$key->to_string(1)} = $value;
+        $hash{make_key($key)} = $value;
     }
     bless { %hash }, $class;
 }
@@ -28,11 +32,19 @@ method to_string($readable = 0) {
 }
 
 method get($key) {
-    return $self->{$key->to_string(1)} // MAL::Object->nil;
+    return $self->{make_key($key)} // MAL::Object->nil;
 }
 
 method contains($key) {
-    return exists $self->{$key->to_string(1)};
+    return exists $self->{make_key($key)};
+}
+
+method get_keys {
+    return sort keys %$self
+}
+
+method get_vals {
+    return map { $self->{$_} } $self->get_keys;
 }
 
 method map_values($f) {
@@ -43,10 +55,20 @@ method map_values($f) {
     return bless { %hash }, ref $self;
 }
 
-method augment($key, $value) {
+method assoc(@pairs) {
     my %hash = %$self;
-    $hash{$key->to_string(1)} = $value;
+    while (@pairs) {
+        my ($key, $value) = splice(@pairs, 0, 2);
+        $hash{make_key($key)} = $value;
+    }
     return bless { %hash }, ref $self;
 }
+
+method dissoc(@keys) {
+    my %hash = %$self;
+    delete @hash{ map { make_key($_) } @keys };
+    return bless { %hash }, ref $self;
+}
+
 
 1;
