606a8277d6a1ef8fc8dddad68865f52a24175241
Stephen Thirlwall <sdt@dr.com>
2015-03-14 23:10:06 +1100

    Add type checks to many builtins

---
+++ sdt-perl/lib/MAL/Object/Applyable.pm
@@ -0,0 +1,14 @@
+package MAL::Object::Applyable;
+use 5.20.0;
+use warnings;
+
+use Function::Parameters qw( :strict );
+
+use parent qw( MAL::Object );
+
+#method new($class: $value) {
+#    die "Cannot create $class items directly\n" if $class eq __PACKAGE__;
+#    bless \$value, $class;
+#}
+
+1;
