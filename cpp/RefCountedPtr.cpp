71b454ac3e2e66012a72bb1bf3d3e219f8ab77c9
Stephen Thirlwall <sdt@dr.com>
2015-03-30 08:03:23 +1100

    WIP memory report

--- cpp/RefCountedPtr.cpp
+++ cpp/RefCountedPtr.cpp
@@ -2,6 +2,28 @@
 
 #if DEBUG_MEMORY_AUDITING
 
+#include "String.h"
+
 RefCounted::Set* RefCounted::s_tracker;
+
+void RefCounted::memoryReport(const RefCounted* root)
+{
+    static int mark = 0;
+    mark++;
+
+    root->mark(mark);
+
+    int unreachableObjectCount = 0;
+    for (auto &it : *s_tracker) {
+        if (it->getMark() != mark) {
+            unreachableObjectCount++;
+
+            TRACE("Unreachable object at %p\n", it);
+        }
+    }
+
+    TRACE("%d unreachable object%s total.\n",
+        unreachableObjectCount, PLURAL(unreachableObjectCount));
+}
 
 #endif
