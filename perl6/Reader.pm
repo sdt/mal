fa1c294b87a831342208addc304620d3bd59b98a
Stephen Thirlwall <sdt@dr.com>
2015-04-11 17:57:56 +1000

    Working reader ?

--- perl6/Reader.pm
+++ perl6/Reader.pm
@@ -5,106 +5,42 @@ use v6;
 use Types;
 
 grammar MALGrammar {
-    rule TOP { <ws>? <form>? <comment>? }
+    rule TOP            { <ws>? <form>? <comment>? }
 
-    rule form { <seq> | <atom> }
+    rule form           { <seq> | <atom> }
 
-    rule seq { <seq-begin> <form>* <seq-end> }
+    rule seq            { <seq-begin> <form>* <seq-end> }
 
-    token comment   { \; .* $$ }
-    token ws        { <[ \s \, ]>* }
-    token seq-begin { < [ ( { > }
-    token seq-end   { < ] ) } > | $ }
+    token seq-begin     { < [ ( { > }
+    token seq-end       { < ] ) } > | $ }
 
-    token atom { <integer> | <string> }
+    token atom          { <integer> | <macro> | <string> | <word> }
 
-    token integer { < + - >? \d+ }
+    token integer       { < + - >? \d+ }
 
-    token string  { <string-begin> <string-body> <string-end> }
-    token string-begin { \" }
-    token string-body  { [ \\ . | <-["]> ]* }
-    token string-end   { \" | $ }
-}
+    rule macro          { <macro-prefix> <form> }
+    token macro-prefix  { < ~@ ' ` ~ @ > }
 
-#`<<
-grammar MALGrammar {
-    rule TOP { <ws>? <form>? <comment>? | { self.panic($/, "What?") }}
-
-    rule form { <s-expr> | <vector> | <hash> | <atom> | { self.panic($/, "Huh?")} }
-
-    rule s-expr { '(' <form>* ')' | { die "expected ')', got EOF" } }
-    rule vector { '[' <form>* ']' | { die "expected ']', got EOF" } }
-    rule hash   { '{' <form>* '}' }
-
-    token atom { <macro> | <string> | <integer> | <word> }
-
-    rule macro   { <macro-prefix> <form> }
-    token macro-prefix { < ~@ ' ` ~ @ > }
+    token string        { <string-begin> <string-body> <string-end> }
+    token string-begin  { \" }
+    token string-body   { [ \\ . | <-["]> ]* }
+    token string-end    { \" | $ }
 
 #    rule with-meta { \^ <form> <form> }
 
-    token word    { <[ \S ] - [ \[ \] \{ \} \( \) \' \" \` \, \; \) ]>+ }
-    token integer { < + - >? \d+ }
-    token string  { \" [ \\ . | <-["]> ]* \" }
+    token word          { <[ \S ] - [ \[ \] \{ \} \( \) \' \" \` \, \; \) ]>+ }
 
-    token comment { \; .* $$ }
-    token ws      { <[ \s \, ]>* }
-
-    method panic($/, $msg) {
-        my $c = $/.CURSOR;
-        my $pos := $c.pos;
-        die "$msg found at pos $pos";
-    }
+    token comment       { \; .* $$ }
+    token ws            { <[ \s \, ]>* }
 }
->>
 
 class MALGrammar::Actions {
-    method string($/) {
-        if $<string-end>.Str eq '' {
-            die "Expected \", got EOF";
-        }
-        make Value.new(type => String, value => $<string-body>.Str);
+    method TOP($/) {
+        make $<form>.ast;
     }
 
-    method integer($/) {
-        make Value.new(type => Integer, value => $/.Int);
-    }
-
-    method word($/) {
-        my $value = $/.Str;
-        my $type = $value.substr(0, 1) eq ':' ?? Keyword !! Symbol;
-        make Value.new(type => $type, value => $value);
-    }
-
-    method macro($/) {
-        my %macro = q/@/  => 'deref',
-                    q/`/  => 'quasiquote',
-                    q/'/  => 'quote',
-                    q/~@/ => 'splice-unquote',
-                    q/~/  => 'unquote',
-                    ;
-
-        my $prefix = $<macro-prefix>.Str;
-        make Value.new(type => List, value => (
-                Value.new(type => Symbol, value => %macro{$prefix}),
-                $<form>.ast
-            ));
-    }
-
-    method atom($/) {
+    method form($/) {
         make $/.values[0].ast;
-    }
-
-    method s-expr($/) {
-        make Value.new(type => List, value => $<form>.map({$_.ast}));
-    }
-
-    method hash($/) {
-        make Value.new(type => HashMap, value => $<form>».ast);
-    }
-
-    method vector($/) {
-        make Value.new(type => Vector, value => $<form>».ast);
     }
 
     method seq($/) {
@@ -124,12 +60,40 @@ class MALGrammar::Actions {
         make Value.new(type => $info<type>, value => $<form>.map({$_.ast}));
     }
 
-    method form($/) {
+    method atom($/) {
         make $/.values[0].ast;
     }
 
-    method TOP($/) {
-        make $<form>.ast;
+    method integer($/) {
+        make Value.new(type => Integer, value => $/.Int);
+    }
+
+    method macro($/) {
+        my %macro = q/@/  => 'deref',
+                    q/`/  => 'quasiquote',
+                    q/'/  => 'quote',
+                    q/~@/ => 'splice-unquote',
+                    q/~/  => 'unquote',
+                    ;
+
+        my $prefix = $<macro-prefix>.Str;
+        make Value.new(type => List, value => (
+                Value.new(type => Symbol, value => %macro{$prefix}),
+                $<form>.ast
+            ));
+    }
+
+    method string($/) {
+        if $<string-end>.Str eq '' {
+            die "Expected \", got EOF";
+        }
+        make Value.new(type => String, value => $<string-body>.Str);
+    }
+
+    method word($/) {
+        my $value = $/.Str;
+        my $type = $value.substr(0, 1) eq ':' ?? Keyword !! Symbol;
+        make Value.new(type => $type, value => $value);
     }
 }
 
