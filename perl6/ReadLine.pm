55f7e4110909440ade9946d6f77ec06037d6c12b
Stephen Thirlwall <sdt@dr.com>
2016-02-10 11:06:22 +1100

    Switch from nativecall readline to linenoise

--- perl6/ReadLine.pm
+++ perl6/ReadLine.pm
@@ -1,35 +1,18 @@
 unit module ReadLine;
 
 use v6;
-
-constant READLINE_LIB = %*ENV<MAL_READLINE> // 'libreadline';
-
-# Import some native functions from GNU readline.
-use NativeCall;
-sub readline(Str $prompt) returns Str
-    is native(READLINE_LIB) { ... }
-sub add_history(Str $line)
-    is native(READLINE_LIB) { ... }
-sub append_history(Int $lineno, Str $filename) returns Int
-    is native(READLINE_LIB) { ... }
-sub read_history(Str $filename) returns Int
-    is native(READLINE_LIB) { ... }
+use Linenoise;
 
 # Load up the .mal-history file
 my $history-path = %*ENV<MAL_HISTORY> // %*ENV<HOME> ~ '/.mal-history';
-if ($history-path.IO ~~ :e) {
-    read_history($history-path);
-}
-else {
-    open($history-path, :w).close(); # Not sure why this is necessary
-}
+linenoiseHistoryLoad($history-path);
 
 # Our single export. Returns a string, or undef on EOF.
 sub read-line(Str $prompt) is export {
-    my $line = readline($prompt);
+    my $line = linenoise($prompt);
     if (defined $line) {
-        add_history($line);
-        append_history(1, $history-path);
+        linenoiseHistoryAdd($line);
+        linenoiseHistorySave($history-path);
     }
     return $line;
 }
